2e90bba8ba48ecfc1af3bf2a5bdff72330f2979e
ameliadenney <amelia.denney@gmail.com>
2026-02-01 08:31:17 +0000

    Beginning to incorporate Transformer

--- model.py
+++ model.py
@@ -2,16 +2,13 @@ import torch
 from torch_geometric.nn import GCNConv
 import torch.nn as nn
 
-
 class GNN(torch.nn.Module):
-
     def __init__ (self, input_channels, hidden_channels, output_channels):
         super().__init__()
         self.conv1 = GCNConv(input_channels, hidden_channels)
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.conv3 = GCNConv(hidden_channels, output_channels)
  
-
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
         x = torch.relu(x)
@@ -19,24 +16,45 @@ class GNN(torch.nn.Module):
         x = torch.relu(x)
         x = self.conv3(x, edge_index)
         x = torch.relu(x)
-        #x = torch.log_softmax(x, dim=1 )
         return x
-    
    
-    
-
 class Transformer(torch.nn.Module):
     def __init__(self, embed_dim, num_heads, num_layers):
-        self.trans = nn.TransformerEncoderLayer(d_model=embed_dim,
-            nhead=num_heads,
-            batch_first=True)
-
+        super().__init__()
+        self.trans = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads, batch_first=True)
+    
+    def forward(self, x):
+        x = self.trans(x)
+        return x 
 
 class SpatioTemporalBlock(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, num_heads):
-        self.spatialBlock = GNN(in_channels, hidden_channels)
+    def __init__(self, input_channels, hidden_channels, output_channels, num_heads):
+        super().__init__()
+        self.spatialBlock = GNN(input_channels, hidden_channels, output_channels)
         self.temporalBlock = Transformer(hidden_channels, num_heads, 1)
 
+    def forward(self, x, edge_index):
+        x = self.spatialBlock(x, edge_index)
+        x = self.reshape_to_trans(x)
+        x = self.temporalBlock(x)
+        x = self.reshape_to_graph(x)
+        return x
+    
+    def reshape_to_trans(self, x):
+        x = x.unsqueeze(-1)
+        x = x.repeat(1, 1, self.embed_dim)
+        return x
+    
+    def reshape_to_graph(self,x):
+        return x
+
 class Model(torch.nn.Module):
-    def __init__(self, num_blocks, in_channels, hidden_channels, num_heads):
-        self
+    def __init__(self, input_channels, hidden_channels, output_channels):
+        super().__init__()
+        self.GNN = GNN(input_channels, hidden_channels, output_channels)
+        self.prediction = nn.Linear(hidden_channels, output_channels)
+
+    def forward(self, x, edge_index):
+        x = self.GNN(x, edge_index)
+        x = self.prediction(x)
+        return x
