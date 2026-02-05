3c91a2923325c19a9af3cc0dd590934e73331136
ameliadenney <amelia.denney@gmail.com>
2026-02-05 15:21:55 +0000

    Added docstrings for model file, and completed transformer code

--- model.py
+++ model.py
@@ -12,6 +12,14 @@ class GNN(torch.nn.Module):
         self.conv3 = GCNConv(hidden_channels, output_channels)
  
     def forward(self, x, edge_index):
+        """
+        Parameters:
+            x (Tensor): The network nodes and features with shape [num_nodes, num_timesteps]
+            edge_index (Tensor): The relationships between nodes
+
+        Returns
+            Tensor: The output embedding with shape [num_nodes, num_timesteps]
+        """
         x = self.conv1(x, edge_index)
         x = torch.relu(x)
         x = self.conv2(x, edge_index)
@@ -20,8 +28,12 @@ class GNN(torch.nn.Module):
         x = torch.relu(x)
         return x
 
-# Positional Encoding - https://pytorch.org/tutorials/beginner/transformer_tutorial.html
 class PositionalEncoding(torch.nn.Module):
+    """
+    Implementing sinusoidal positional encoding, based on the approach used in 'Attention is all you need'
+
+    Code for this class has been taken from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+    """
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
         position = torch.arange(max_len).unsqueeze(1)
@@ -36,19 +48,44 @@ class PositionalEncoding(torch.nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x: Tensor) -> Tensor:
+        """
+        Parameters:
+            x (Tensor): The network nodes and features with shape [num_nodes, num_timesteps]
+
+        Returns
+            Tensor: The network nodes and featureswith added positional encoding, with shape [num_nodes, num_timesteps]
+        """
         x = x + self.pe[:x.size(0)]
         x = self.dropout(x)
         return x
 
    
 class TemporalTransformer(torch.nn.Module):
-    def __init__(self, num_heads, num_layers, embed_dim = 16, device = 'cpu'):
+    """
+    Transformer component which takes a timeseries sequence, and forecasts future values for it
+
+    Parts of the component:
+        -   Preprocessing: reformating the input from shape [num_nodes, num_timesteps] to [num_nodes, num_timesteps, embed_dim]
+        -   Positional encoding: Adding information such that the model knows the order of the time series
+        -   Computing attention mask: ensuring the model knows which timesteps can pay attention to which other ones
+        -   Transformer layer: Computes predictions for the masked nodes
+        -   Postprocessing: reformatting the output back to shape [num_nodes, num_timesteps] by reducing the feature dimension back to 1
+    """
+    def __init__(self, num_heads, embed_dim, device = 'cpu'):
+        """
+        Parameters:
+            num_heads (int): The number of attention head the transformer encoder uses
+            embed_dim (int): Dimensionality of the embedding space representing each timestep of the input data
+            device (str): The device that the model is run on
+        """
+
         super().__init__()
         # Class parameters
         self.device = device
         self.context_window = hp.CONTEXT_WINDOW
         self.forecast_window = hp.FORECAST_WINDOW
         self.total_window = self.context_window + self.forecast_window
+        
 
         # Preprocessing layers
         self.input_embedding  = nn.Linear(1, embed_dim)
@@ -61,6 +98,13 @@ class TemporalTransformer(torch.nn.Module):
         self.fcn = nn.Linear(embed_dim, 1)
     
     def forward(self, x):
+        """
+        Parameters:
+            x (Tensor): The network nodes and features with shape [num_nodes, num_timesteps]
+
+        Returns
+            Tensor: The output embedding with shape [num_nodes, num_timesteps]
+        """
         # Adding an extra dimension so it is (batch_size, seq_len, 1)
         x = x.unsqueeze(-1)
         # Increasing the feature dimensions from 1 to embed_dim
@@ -71,7 +115,7 @@ class TemporalTransformer(torch.nn.Module):
         # Creating an attention mask, so that the model cannot see future time steps
         attention_mask = self.generate_attention_mask() 
         # Passing through a transformer layer
-        x = self.trans(x, mask=attention_mask)
+        x = self.trans(x, src_mask=attention_mask)
 
         # Reducing the feature dimensions back to 1
         x = self.fcn(x)
@@ -86,23 +130,39 @@ class TemporalTransformer(torch.nn.Module):
         return mask
 
 class SpatioTemporalBlock(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, output_channels, num_heads):
+    def __init__(self, input_channels, hidden_channels, output_channels, num_heads, embed_dim):
         super().__init__()
         self.spatialBlock = GNN(input_channels, hidden_channels, output_channels)
-        self.temporalBlock = TemporalTransformer(hidden_channels, num_heads, 1)
+        self.temporalBlock = TemporalTransformer(num_heads, embed_dim)
 
     def forward(self, x, edge_index):
+        """
+        Parameters:
+            x (Tensor): The network nodes and features with shape [num_nodes, num_timesteps]
+            edge_index (Tensor): The relationships between nodes
+
+        Returns
+            Tensor: The output embedding with shape [num_nodes, num_timesteps]
+        """
         x = self.spatialBlock(x, edge_index)
         x = self.temporalBlock(x)
         return x
 
 class Model(torch.nn.Module):
-    def __init__(self, input_channels, hidden_channels, output_channels):
+    def __init__(self, input_channels, hidden_channels, output_channels, num_heads, embed_dim):
         super().__init__()
-        self.spatio_temporal = SpatioTemporalBlock(input_channels, hidden_channels, output_channels)
+        self.spatio_temporal = SpatioTemporalBlock(input_channels, hidden_channels, output_channels, num_heads, embed_dim)
         self.prediction = nn.Linear(hidden_channels, output_channels)
 
     def forward(self, x, edge_index):
+        """
+        Parameters:
+            x (Tensor): The network nodes and features with shape [num_nodes, num_timesteps]
+            edge_index (Tensor): The relationships between nodes
+
+        Returns
+            Tensor: The output embedding with shape [num_nodes, num_timesteps]
+        """
         x = self.spatio_temporal(x, edge_index)
         x = self.prediction(x)
         return x
